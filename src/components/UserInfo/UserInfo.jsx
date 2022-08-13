d1048a5ed665ba9f9831bfb014171194bc69bdfa
starnizar <nizarincorp@gmail.com>
2022-08-13 17:27:14 +0000

    user info refactoring

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -4,13 +4,18 @@ import './UserInfo.scss'
 const UserInfo = ({user}) => {
     console.log();
     return <div className="userInfo">
-        <img src={user && user.avatar_url} alt="" />
-        <h1>{user && user.name}</h1>
-        <a href={user && user.html_url} target="_blank" rel="noreferrer">{user && user.login}</a>
-        <div className='follows'>
-            <p><i className="fa fa-users"></i>{user && user.followers} followers</p>
-            <p><i className="fa fa-user"></i>{user && user.following} following</p>
-        </div>
+        {
+            user && <>
+                <img src={user.avatar_url} alt="" />
+                <h1>{user.name}</h1>
+                <a href={user.html_url} target="_blank" rel="noreferrer">{user && user.login}</a>
+                <div className='follows'>
+                    <p><i className="fa fa-users"></i>{user.followers} followers</p>
+                    <p><i className="fa fa-user"></i>{user.following} following</p>
+                </div>
+            </>
+        }
+        
     </div>
 }
 
